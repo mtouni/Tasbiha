7469d7f2fc7ee124bfaed87c6aaa5e60e198100a
amrReda95 <amr.reda151@gmail.com>
2016-04-06 11:54:06 +0200

    demo

--- app/src/main/java/com/mal/amr/tasbiha/MainActivity.java
+++ app/src/main/java/com/mal/amr/tasbiha/MainActivity.java
@@ -1,9 +1,9 @@
 package com.mal.amr.tasbiha;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.mal.amr.tasbiha.adapters.MainPagerAdapter;
--- app/src/main/java/com/mal/amr/tasbiha/fragments/Tasbih.java
+++ app/src/main/java/com/mal/amr/tasbiha/fragments/Tasbih.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,10 +17,39 @@ import com.mal.amr.tasbiha.R;
  */
 public class Tasbih extends Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_tasbih_layout, container, false);
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.tasbih_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.baad_alsalah:
+                item.setChecked(true);
+                break;
+            case R.id.zekr_hor:
+                item.setChecked(true);
+                break;
+            case R.id.gheer_mohadad:
+                item.setChecked(true);
+                break;
+        }
+        return true;
     }
 }
