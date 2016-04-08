9b4984059270514275383bffdf0a1898d9763786
amrReda95 <amr.reda151@gmail.com>
2016-04-08 23:47:41 +0200

    demo

---
+++ app/src/main/java/com/mal/amr/tasbiha/db/Contract.java
@@ -0,0 +1,16 @@
+package com.mal.amr.tasbiha.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Amr on 4/8/2016.
+ */
+public class Contract implements BaseColumns {
+    public static final String TABLE_NAME = "tasbihah";
+
+    public static final String FREE = "free";
+    public static final String SOBHAN_ALLAH = "sobhan_allah";
+    public static final String ALHAMDULELLAH = "alhamdulellah";
+    public static final String ALLAH_AKBAR = "allah_akbar";
+    public static final String DATE = "date";
+}
---
+++ app/src/main/java/com/mal/amr/tasbiha/db/DBHelper.java
@@ -0,0 +1,38 @@
+package com.mal.amr.tasbiha.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Amr on 4/8/2016.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "tasbihah.db";
+    public static final int VERSION = 1;
+
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String sql = "create table " + Contract.TABLE_NAME +
+                " (" + Contract._ID + " integer primary key, " +
+                Contract.FREE + " integer not null, " +
+                Contract.SOBHAN_ALLAH + " integer not null, " +
+                Contract.ALHAMDULELLAH + " integer not null, " +
+                Contract.ALLAH_AKBAR + " integer not null, " +
+                Contract.DATE + " text unique not null);";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + Contract.TABLE_NAME);
+
+        onCreate(db);
+    }
+}
