2de78bbeb5986fca60f11ff9da7e862114949530
amrReda95 <amr.reda151@gmail.com>
2016-04-11 19:39:20 +0200

    demo

--- app/src/main/java/com/mal/amr/tasbiha/fragments/Ehsaa.java
+++ app/src/main/java/com/mal/amr/tasbiha/fragments/Ehsaa.java
@@ -9,9 +9,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
-import android.widget.Toast;
 
 import com.mal.amr.tasbiha.R;
+
+import java.util.Calendar;
 
 /**
  * Created by Amr on 4/5/2016.
@@ -19,6 +20,10 @@ import com.mal.amr.tasbiha.R;
 public class Ehsaa extends Fragment {
 
     CalendarView calendarView;
+    Calendar calendar = Calendar.getInstance();
+    String whereArg = calendar.get(Calendar.DAY_OF_MONTH)
+            + "/" + calendar.get(Calendar.MONTH)
+            + "/" + calendar.get(Calendar.YEAR);
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,15 +37,34 @@ public class Ehsaa extends Fragment {
 
         View v = inflater.inflate(R.layout.ehsaa_layout, container, false);
 
-        CalendarView calendar = (CalendarView) v.findViewById(R.id.calendarView);
-        calendar.setShowWeekNumber(true);
+        calendarView = (CalendarView) v.findViewById(R.id.calendarView);
+        calendarView.setShowWeekNumber(false);
 
-        calendar.setFirstDayOfWeek(7);
+        calendarView.setFirstDayOfWeek(7);
 
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        Calendar minCalndarViewLimit = Calendar.getInstance();
+        minCalndarViewLimit.set(Calendar.SECOND, 0);
+        minCalndarViewLimit.set(Calendar.MINUTE, 0);
+        minCalndarViewLimit.set(Calendar.HOUR, 0);
+        minCalndarViewLimit.set(Calendar.DAY_OF_MONTH, 1);
+        minCalndarViewLimit.set(Calendar.MONTH, minCalndarViewLimit.get(Calendar.MONTH));
+        minCalndarViewLimit.set(Calendar.YEAR, minCalndarViewLimit.get(Calendar.YEAR));
+
+        Calendar maxCalndarViewLimit = Calendar.getInstance();
+        maxCalndarViewLimit.set(Calendar.SECOND, 0);
+        maxCalndarViewLimit.set(Calendar.MINUTE, 0);
+        maxCalndarViewLimit.set(Calendar.HOUR, 0);
+        maxCalndarViewLimit.set(Calendar.DAY_OF_MONTH, 31);
+        maxCalndarViewLimit.set(Calendar.MONTH, maxCalndarViewLimit.get(Calendar.MONTH));
+        maxCalndarViewLimit.set(Calendar.YEAR, maxCalndarViewLimit.get(Calendar.YEAR));
+
+        calendarView.setMinDate(minCalndarViewLimit.getTimeInMillis());
+        calendarView.setMaxDate(maxCalndarViewLimit.getTimeInMillis());
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getContext(), dayOfMonth + "/" + (month + 1) + "/" + year, Toast.LENGTH_LONG).show();
+
             }
         });
         return v;
@@ -56,8 +80,19 @@ public class Ehsaa extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //if (item.getItemId() == R.id.up_or_down)
-            //here to reset the fragment
+        if (item.getItemId() == R.id.up_or_down) {
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            int month = calendar.get(Calendar.MONTH);
+            int year = calendar.get(Calendar.YEAR);
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, day);
+
+            long millitTime = calendar.getTimeInMillis();
+            calendarView.setDate(millitTime);
+
+
+        }
 
         return false;
     }
